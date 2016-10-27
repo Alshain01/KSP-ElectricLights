d09b0cb35c5d59542dfe84b73d668ac82cd97325
Alshain01 <alshain01@suddenlink.net>
2016-10-26 22:43:10 -0500

    Initial Commit

---
+++ ElectricLights/ModuleColorChangerConsumer.cs
@@ -0,0 +1,27 @@
+﻿namespace ElectricLights
+{
+    public class ModuleColorChangerConsumer : ModuleColorChanger
+    {
+        [KSPField]
+        public float resourceRate = 1.0F;
+        [KSPField]
+        public double resourceAmount = 0.02;
+        [KSPField]
+        public string resourceType = "ElectricCharge";
+
+        public override void OnUpdate()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+                {
+                if (this.animState)
+                {
+                    if (this.part.RequestResource(resourceType, resourceAmount * resourceRate * TimeWarp.deltaTime) <= 0)
+                    {
+                        this.SetState(false);
+                    }
+                }
+            }
+            base.OnUpdate();
+        }
+    }
+}
